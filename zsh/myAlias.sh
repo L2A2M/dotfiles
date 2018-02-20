0d016ffd1156284279f0de761a6633dd2bb3ed0b
enochs <ewsuen@gmail.com>
2018-02-19 21:44:51 -0600

    Changed IP to vps

--- zsh/myAlias.sh
+++ zsh/myAlias.sh
@@ -20,7 +20,7 @@ alias s3l='aws s3 ls tedwindow.com --recursive'
 alias sync2s3='cd ~/hugo/my-website && aws s3 sync public s3://tedwindow.com --cache-control max-age=3600 --delete'
 alias post='cd ~/hugo/my-website/content/post'
 alias go='cd ~/hugo/my-website && rm -r public && hugo'
-alias vps1='ssh -i ~/Shared/Hugo/LightsailDefaultPrivateKey.pem ubuntu@52.5.68.133'
+alias vps1='ssh -i ~/Dropbox/Shared/Hugo/LightsailDefaultPrivateKey.pem ubuntu@34.234.122.181'
 
 #myfunc () { cd "$@"; ls -a; }
 #alias cd=myfunc
